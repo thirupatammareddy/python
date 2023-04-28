085faffddb3b6135dbfe610643cff38f6fd582fe
Chaitanya Reddy <thirupatammakallam@gmail.com>
2023-04-28 19:55:41 +0530

    reddy

---
+++ 6q.py
@@ -0,0 +1,8 @@
+import math
+numbers=input("provideD:")
+numbers=numbers.split(',')
+result_list=[]
+for Din numbers:
+    Q=round(math.sqrt(2*50int(D)/30))
+    result_list.append(Q)
+    print(result_list)
---
+++ 8q.py
@@ -0,0 +1,4 @@
+phrase=input("inut words:")
+phrase_list=phrase.split(",")
+phrase_list.sort()
+print((',').join(phrase_list))
