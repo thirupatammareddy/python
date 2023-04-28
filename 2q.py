1dea8389899535fa90ed2bc745fadad0d0dacaec
Chaitanya Reddy <thirupatammakallam@gmail.com>
2023-04-28 17:34:03 +0530

    hi hello

---
+++ 2q.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    value=1
+    for i in range(1,n+1):
+        value= value *i
+        return value
+    number=int(input("type a number:")
+               print(factorial(number))
+          
