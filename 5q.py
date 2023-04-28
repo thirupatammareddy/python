39bcf950ff08cca63434faa0e08b4fa770b3f917
Chaitanya Reddy <thirupatammakallam@gmail.com>
2023-04-28 19:18:17 +0530

    reddy

---
+++ 5q.py
@@ -0,0 +1,11 @@
+class 10 string():
+    def_init_(self):
+        self.str1=" "
+     def get_string(self):
+         self.str1=input()
+         print_string(self):
+             print(self.str1.upper())
+             //str=10string()
+               str.get_string()
+               str.print_string()//
+             
